a2640d7ea703b2fa8d6be5d370e6edb9ef672f9a
YAWAL <yavorskyyval@gmail.com>
2017-07-07 21:04:03 +0300

    training

--- src/task/ServerTask/nativeServer/main/server.go
+++ src/task/ServerTask/nativeServer/main/server.go
@@ -11,6 +11,7 @@ func main() {
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 //return text after / in URL Path
+//test test test
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "{\n \"name\":%q \n}", r.URL.Path)
 }
