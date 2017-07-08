8a9813759ec09bd393409fa07cc211bd975b9a01
YAWAL <yavorskyyval@gmail.com>
2017-07-08 11:04:10 +0300

    training

---
+++ src/task/Task2/main/task243_test.go
@@ -0,0 +1,38 @@
+package main
+
+import "testing"
+
+func Test1SearchAllRoots(t *testing.T)  {
+	inputValues := []int{5, 8, 18, 41,57677}
+	for _, value := range inputValues{
+		if SearchAllRoots(value){
+			t.Log("Function works properly with correct n-values")
+		}
+	}
+}
+
+func Test2SearchAllRoots(t *testing.T)  {
+	inputValues := []int{3, 4, 7, 9, 11}
+	for _, value := range inputValues{
+		if !SearchAllRoots(value){
+			t.Log("Function works properly with not correct n-values")
+		}
+	}
+}
+func Test1SearchOneRoot(t *testing.T)  {
+	inputValues := []int{5, 8, 18, 41,57677}
+	for _, value := range inputValues{
+		if SearchOneRoot(value){
+			t.Log("Function works properly with correct n-values")
+		}
+	}
+}
+
+func Test2SearchOneRoot(t *testing.T)  {
+	inputValues := []int{3, 4, 7, 9, 11}
+	for _, value := range inputValues{
+		if !SearchOneRoot(value){
+			t.Log("Function works properly with not correct n-values")
+		}
+	}
+}
